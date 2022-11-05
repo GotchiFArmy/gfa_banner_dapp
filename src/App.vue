7de119d8b66de3e9ced504f9dbc0fff29338fbaf
Philippe MICHEL <fifoooo@gmail.com>
2022-11-05 22:32:08 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import LayoutHeader from './components/LayoutHeader.vue'
+import { MetaMaskConnector, WalletConnectConnector, CoinbaseWalletConnector } from 'vue-dapp'
+
+const infuraId = ''
+
+const connectors = [
+  new MetaMaskConnector({
+    appUrl: 'http://localhost:3000',
+  }),
+  new WalletConnectConnector({
+    qrcode: true,
+    rpc: {
+      1: `https://mainnet.infura.io/v3/${infuraId}`,
+      4: `https://rinkeby.infura.io/v3/${infuraId}`,
+    },
+  }),
+  new CoinbaseWalletConnector({
+    appName: 'Vue Dapp',
+    jsonRpcUrl: `https://mainnet.infura.io/v3/${infuraId}`,
+  }),
+]
+</script>
+
+<template>
+  <layout-header />
+
+  <router-view></router-view>
+
+  <vd-board :connectors="connectors" dark />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'virtual:windi.css'
+import './index.css'
+import { VueDapp } from 'vue-dapp'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(VueDapp)
+
+app.mount('#app')
